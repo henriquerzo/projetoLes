afd26bf6b86de591ef554fd330020089133a3fe0
Henrique César Florêncio <henriquerzo@gmail.com>
2013-07-22 09:44:18 -0300

    Ultima versão

--- LesProjectPrincipal/src/com/example/lesprojectprincipal/MainActivity.java
+++ LesProjectPrincipal/src/com/example/lesprojectprincipal/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends Activity {
 	}
 	
 	public void jogar(View view){
-		Intent intent = new Intent(this, SliderActivity.class);
+		Intent intent = new Intent(this, EscolhaDeImagem.class);
 		startActivity(intent);
 	}
 
--- LesProjectPrincipal/src/com/example/lesprojectprincipal/Vencedor.java
+++ LesProjectPrincipal/src/com/example/lesprojectprincipal/Vencedor.java
@@ -1,37 +1,28 @@
 package com.example.lesprojectprincipal;
 
-
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
-import android.view.Window;
 
-public class MenuActivity extends Activity {
+public class Vencedor extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.activity_menu);
-		
-		
+		setContentView(R.layout.activity_vencedor);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu, menu);
+		getMenuInflater().inflate(R.menu.vencedor, menu);
 		return true;
 	}
-	
-	
-	
-	
-	public void finishDialog(View v) {
-        MenuActivity.this.finish();
-    }
 
+	public void voltar(View view){
+		Intent intent = new Intent(this, EscolhaDeImagem.class);
+		startActivity(intent);
+	}
 }
